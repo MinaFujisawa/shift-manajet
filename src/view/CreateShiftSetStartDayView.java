0ddd1eef66a48e7f7cff5851f5869e680b3ea7a5
MinaFujisawa <fujimina37@gmail.com>
2017-05-08 19:46:40 -0700

    add current date and adjust styles for CreateShiftSetStartDay

--- src/view/CreateShiftSetStartDayView.java
+++ src/view/CreateShiftSetStartDayView.java
@@ -2,7 +2,7 @@
  * Created by MinaFujisawa on 2017/05/04.
  */
 package view;
-import javafx.application.Application;
+import controller.CreateShiftSetStartDayController;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -10,22 +10,16 @@ import javafx.scene.control.DatePicker;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.geometry.Pos;
-import javafx.scene.layout.VBox;
-import com.sun.javafx.scene.control.skin.DatePickerSkin;
-
 import java.net.URL;
 import java.time.LocalDate;
 
 
 public class CreateShiftSetStartDayView extends View {
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+    private CreateShiftSetStartDayController controller = new CreateShiftSetStartDayController();
 
     @Override
     public void start(Stage stage) {
@@ -39,25 +33,33 @@ public class CreateShiftSetStartDayView extends View {
             Label heading = new Label("SET THE START DATE");
             Label subHeading = new Label("This shift will start at");
             DatePicker checkInDatePicker = new DatePicker();
+
+            //sets default date
+            checkInDatePicker.setValue(controller.currentDate());
+
             Button btn = new Button("START CREATING THE SHIFT");
 
             //sets styles
             heading.setFont(Font.font(22));
             subHeading.setFont(Font.font(14));
-            VBox.setMargin( subHeading , new Insets( 40,0,0,0 ) );
-            VBox.setMargin( checkInDatePicker , new Insets( 25,0,50,0 ) );
+            VBox.setMargin(subHeading, new Insets(40, 0, 0, 0));
+            VBox.setMargin(checkInDatePicker, new Insets(25, 0, 50, 0));
 
             // adds components to the vboxes
             addNavigationBar(vboxContainer);
-            vboxContents.getChildren().addAll( heading , subHeading, checkInDatePicker, btn );
+            vboxContents.getChildren().addAll(heading, subHeading, checkInDatePicker, btn);
 
             // sets alignment
             vboxContainer.setAlignment(Pos.TOP_CENTER);
             vboxContents.setAlignment(Pos.CENTER);
 
             // sets components
-            pane.setTop( vboxContainer );
-            pane.setCenter( vboxContents );
+            pane.setTop(vboxContainer);
+            pane.setCenter(vboxContents);
+
+            //
+            LocalDate startDate = checkInDatePicker.getValue();
+            System.out.println(startDate);
 
             // creates scene
             Scene scene = new Scene(pane, 1280, 800);
@@ -67,12 +69,22 @@ public class CreateShiftSetStartDayView extends View {
 
             pane.setId("set-start-date");
 
+
+//            @Override
+//            public void handle (ActionEvent event){
+//                getStartDate(checkInDatePicker);
+//            }
+
+
             //Add scene to the stage
             stage.setScene(scene);
             //Show stage
             stage.show();
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
+        } {
         }
     }
+
+
 }
