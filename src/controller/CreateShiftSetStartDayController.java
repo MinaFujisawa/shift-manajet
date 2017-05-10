db09d74fe56b705bfdf1acaa8ab1ffda807eda70
MinaFujisawa <fujimina37@gmail.com>
2017-05-09 19:31:03 -0700

    Delete staffAddView and insert it to the ManagerStaffView

--- src/controller/CreateShiftSetStartDayController.java
+++ src/controller/CreateShiftSetStartDayController.java
@@ -1,15 +1,16 @@
 package controller;
 
+import view.CreateShiftSetStartDayView;
+
 import java.sql.Date;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 
 public class CreateShiftSetStartDayController extends Controller {
+
     public LocalDate currentDate (){
-        //DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
         LocalDate localDate = LocalDate.now();
-        //LocalDate localDate = LocalDate.parse(date, formatter);
         return localDate;
     }
 }
--- src/view/ManagerStaffView.java
+++ src/view/ManagerStaffView.java
@@ -2,13 +2,17 @@
  * Created by MinaFujisawa on 2017/05/04.
  */
 package view;
+
 import com.sun.org.apache.xpath.internal.operations.String;
 import controller.ManagerStaffController;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
@@ -27,6 +31,7 @@ import java.net.URL;
 public class ManagerStaffView extends View {
 
     private ManagerStaffController controller = new ManagerStaffController();
+    int index = 2;
 
     @Override
     public void start(Stage stage) {
@@ -36,7 +41,7 @@ public class ManagerStaffView extends View {
             VBox vboxContainer = new VBox();
             GridPane grid = new GridPane();
 
-
+            //creates components
             Text hName = new Text("NAME");
             Text hID = new Text("ID");
             Text hPassword = new Text("PASSWORD");
@@ -44,55 +49,84 @@ public class ManagerStaffView extends View {
             Button addStaffBtn = new Button("Add Staff");
 
             //table layout
-            grid.add( addStaffBtn , 0 , 0 , 4 , 1 );
-            grid.add( hName , 0 , 1 , 1 , 1 );
-            grid.add( hID , 1 , 1 , 1 , 1 );
-            grid.add( hPassword , 2 , 1 , 1 , 1 );
+            grid.add(addStaffBtn, 0, 0, 4, 1);
+            grid.add(hName, 0, 1, 1, 1);
+            grid.add(hID, 1, 1, 1, 1);
+            grid.add(hPassword, 2, 1, 1, 1);
 
-            int index = 2;
-            addPerson(name, grid, index);
-            //grid.add( name , 0 , index , 1 , 1 );
-//            grid.add( name2 , 0 , 3 , 1 , 1 );
+            // method test
+//            Text mina = new Text("Mina");
+//            Text fumin = new Text("Fumin");
+//            addPerson(mina, grid, index);
+//            addPerson(fumin, grid, index);
 
-
-
+            //button
             addStaffBtn.setOnAction(new EventHandler<ActionEvent>() {
                 @Override
                 public void handle(ActionEvent event) {
-                    ManagerStaffAddView msa = new ManagerStaffAddView();
+                    //ManagerStaffAddView msa = new ManagerStaffAddView();
 
                     try {
-                        // 新しいウインドウを生成
-                        Stage newStage = new Stage();
-                        // モーダルウインドウに設定
-                        newStage.initModality(Modality.APPLICATION_MODAL);
+                        // creates modal window
+                        Stage winStage = new Stage();
+                        winStage.initModality(Modality.APPLICATION_MODAL);
+                        winStage.initStyle(StageStyle.UNDECORATED);
+                        winStage.setTitle("Add new staff");
 
-                        newStage.initStyle(StageStyle.UNDECORATED);
-                        newStage.setTitle("ABC");
+                        /****************
+                         * Contents in the modal window
+                         ***************/
+                        //creates root elements
+                        GridPane grid = new GridPane();
+
+                        //creates components
+                        Text hName = new Text("NAME");
+                        Text hPosi = new Text("POSITION");
+                        Button addStaffBtn = new Button("Add Staff");
+                        TextField nameField = new TextField();
+                        ChoiceBox cb = new ChoiceBox(FXCollections.observableArrayList(
+                                "First", "Second", "Third")
+                        );
+
+                        //table layout
+                        grid.add(hName, 0, 0, 1, 1);
+                        grid.add(nameField, 1, 0, 1, 1);
+                        grid.add(hPosi, 0, 1, 1, 1);
+                        grid.add(cb, 1, 1, 1, 1);
+                        grid.add(addStaffBtn, 0, 2, 3, 1);
+
+                        addStaffBtn.setOnAction(new EventHandler<ActionEvent>() {
+                            @Override
+                            public void handle(ActionEvent event) {
+//                                ManagerStaffView ms = new ManagerStaffView();
+
+                                try {
+                                    Text addedName = new Text();
+                                    Text test = new Text("Test");
+                                    addedName.setText(nameField.getText());
+                                    Object position = cb.getSelectionModel().getSelectedItem();
+
+                                    //SEEMS CANNOT ADD TO GRID FROM HERE....
+                                    grid.add(test, 0, 3, 1, 1);
+                                    addPerson(addedName, grid, index);
+                                    winStage.close();
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        });
+
+                        Scene winScene = new Scene(grid, 400, 400);
+                        winStage.setScene(winScene);
+                        winStage.show();
 
 
-//                        newStage.setScene(newScene);
-
-                        // 新しいウインドウ内に配置するコンテンツを生成
-                        HBox hbox = new HBox();
-                        Label label = new Label("登録画面");
-                        label.setFont(new Font(20d));
-                        hbox.getChildren().add(label);
-
-                        newStage.setScene(new Scene(hbox));
-
-                        // 新しいウインドウを表示
-                        newStage.show();
-
-
-                        msa.start(stage);
+                        //msa.start(stage);
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
                 }
             });
-
-
 
 
             //sets styles
@@ -109,15 +143,11 @@ public class ManagerStaffView extends View {
             pane.setTop(vboxContainer);
             pane.setCenter(grid);
 
-
-
             // creates scene
             Scene scene = new Scene(pane, 1280, 800);
 
             //Import general css file
             scene.getStylesheets().add(new URL("file:resources/css/application.css").toExternalForm());
-
-
 
             //Add scene to the stage
             stage.setScene(scene);
@@ -125,31 +155,25 @@ public class ManagerStaffView extends View {
             stage.show();
         } catch (Exception e) {
             e.printStackTrace();
-        } {
+        }
+        {
         }
     }
-    Text name = new Text();
-    public void setName(Text name) {
-        this.name = name;
-    }
-//    public void plusIndex(){
-//        index++;
-//    }
+
+
     public void addPerson(Text name, GridPane grid, int index) {
         Text id = new Text();
         id.setText(controller.getSaltString());
         Text password = new Text();
         password.setText(controller.getSaltString());
-        grid.add( name , 0 , index , 1 , 1 );
-        grid.add( id , 1 , index , 1 , 1 );
-        grid.add( password , 2 , index , 1 , 1 );
-        index++;
+        grid.add(name, 0, index, 1, 1);
+        grid.add(id, 1, index, 1, 1);
+        grid.add(password, 2, index, 1, 1);
+        plusIndex();
     }
 
-//    Scene newScene;
-//    public void setScene(Scene Scene){
-//        newScene = Scene;
-//    }
-
+    public int plusIndex() {
+        return index++;
+    }
 
 }
