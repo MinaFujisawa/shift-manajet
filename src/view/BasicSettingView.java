b1f48b8442755c993acce8212ef5cddbbb3390c5
MinaFujisawa <fujimina37@gmail.com>
2017-05-10 17:10:34 -0700

    Fix layout for BasicSettingView

--- src/view/BasicSettingView.java
+++ src/view/BasicSettingView.java
@@ -8,6 +8,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
@@ -22,76 +23,84 @@ import javafx.stage.Stage;
 import model.Employee;
 import model.Position;
 
+import java.net.URL;
+import java.sql.Date;
+import java.time.LocalDate;
 import java.util.Random;
+
 
 import static com.sun.javafx.tools.resource.DeployResource.Type.data;
 
-public class BasicSettingView extends View{
+public class BasicSettingView extends View {
 
     private BasicSettingController controller = new BasicSettingController();
 
 
     public void start(Stage stage) {
         try {
+            //creates root elements
+            BorderPane pane = new BorderPane();
+            VBox vboxContainer = new VBox();
+            VBox vboxContents = new VBox();
 
-            final TableView<Position> table = new TableView<>();
-            final ObservableList<Position> data =
-                    FXCollections.observableArrayList();
-            final HBox hb = new HBox();
 
+
+            /*
+            * Title
+            */
+            Label title = new Label("BASIC SETTING");
+            title.setFont(new Font(30));
+
+
+            /*
+            * Form
+            */
+
+            //Form for company name
             GridPane grid = new GridPane();
             grid.setAlignment(Pos.CENTER);
             grid.setHgap(10);
             grid.setVgap(10);
-
-                        final VBox vbox = new VBox();
-            vbox.setSpacing(5);
-            vbox.setPadding(new Insets(10, 0, 0, 10));
-
-            grid.setPadding(new Insets(25, 25, 25, 25));
-            Scene scene = new Scene(grid,1184, 775);
-
-
-            // create title field
-            Text title = new Text("BASIC SETTING");
-            title.setFont(new Font("Arial",30));
-            grid.add(title, 1, 1);
-
-
-            // create company name input field
+            grid.setPadding(new Insets(80, 0, 50, 0));
             TextField companyName = new TextField();
-            grid.add(companyName, 1, 2);
             Label labelCompanyName = new Label();
             labelCompanyName.setText("Company name:");
             labelCompanyName.setLabelFor(companyName);
-            grid.add(labelCompanyName, 0, 2);
 
+            grid.add(labelCompanyName, 0, 0, 1, 1);
+            grid.add(companyName, 1, 0, 3, 1);
 
-            //create period type radio button field
-            Label lb = new Label();
-            lb.setText("Period Type:");
-            lb.setLabelFor(companyName);
-            grid.add(lb, 0, 3);
-            final ToggleGroup group = new ToggleGroup();
+            //form for period type
+            ToggleGroup group = new ToggleGroup();
             RadioButton rb1 = new RadioButton("Every month");
             rb1.setToggleGroup(group);
-            grid.add(rb1, 1, 3);
             RadioButton rb2 = new RadioButton("Every week");
             rb2.setToggleGroup(group);
-            grid.add(rb2, 2, 3);
             RadioButton rb3 = new RadioButton("Every 2week");
             rb3.setToggleGroup(group);
-            grid.add(rb3, 3, 3);
+            Label lb = new Label();
+            lb.setText("Period Type:");
+            //lb.setLabelFor(companyName);
+
+            grid.add(lb, 0, 3, 1, 1);
+            grid.add(rb1, 1, 3, 1, 1);
+            grid.add(rb2, 2, 3, 1, 1);
+            grid.add(rb3, 3, 3, 1, 1);
 
 
+            /*
+            * Add shift position
+            */
 
-            // Shift positions
-            final Label label = new Label("Create shift position");
-            label.setFont(new Font("Arial", 20));
+            Label positionTitle = new Label("Create shift position");
+            positionTitle.setFont(new Font(20));
+
+            TableView<String> table = new TableView<>();
+            ObservableList<String> data = FXCollections.observableArrayList();
 
             table.setEditable(true);
 
-            TableColumn<Position, String> positionNameCol = new TableColumn<>("Position name");
+            TableColumn<String, String> positionNameCol = new TableColumn<>("Position name");
             positionNameCol.setMinWidth(400);
             positionNameCol.setCellValueFactory(
                     new PropertyValueFactory<>("name"));
@@ -100,78 +109,93 @@ public class BasicSettingView extends View{
             table.setItems(data);
             table.getColumns().addAll(positionNameCol);
 
-            final TextField addPositionName = new TextField();
+            TextField addPositionName = new TextField();
             addPositionName.setPromptText("Position name");
             addPositionName.setMaxWidth(positionNameCol.getPrefWidth());
 
 
             // add bottom
-            final Button addButton = new Button("Add");
+            Button addButton = new Button("Add");
+            final Long posID;
             addButton.setOnAction((ActionEvent e) -> {
-                data.add(new Position(
-                        addPositionName.getText()));
+                data.add(addPositionName.getText());
                 addPositionName.clear();
             });
 
 
             // delete bottom
-            TableColumn<Position, Position> deleteCol = new TableColumn<>("Delete");
+            TableColumn<String, String> deleteCol = new TableColumn<>("Delete");
             deleteCol.setMinWidth(100);
             deleteCol.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
-            deleteCol.setCellFactory(param -> new TableCell<Position, Position>() {
+            deleteCol.setCellFactory(param -> new TableCell<String, String>() {
                 private final Button deleteButton = new Button("delete");
 
-                @Override
-                protected void updateItem(Position position, boolean empty) {
-                    super.updateItem(position, empty);
-
-                    if (position == null) {
-                        setGraphic(null);
-                        return;
-                    }
-
-                    setGraphic(deleteButton);
-                    deleteButton.setOnAction(event -> data.remove(position));
-                }
+//                @Override
+//                protected void updateItem(Position position, boolean empty) {
+//                    super.updateItem(position, empty);
+//
+//                    if (position == null) {
+//                        setGraphic(null);
+//                        return;
+//                    }
+//
+//                    setGraphic(deleteButton);
+//                    //deleteButton.setOnAction(event -> data.remove(position));
+//                }
             });
 
             table.getColumns().addAll(deleteCol);
 
-            hb.getChildren().addAll(addPositionName, addButton);
-            hb.setSpacing(3);
 
-            grid.add(vbox, 1,6);
-            vbox.getChildren().addAll(label, table, hb);
+            /*
+            * Start button
+            */
 
-
-            // create start button
-            Button btnStart = new Button("Start");
-            HBox hbBtn = new HBox(10);
-            //Set box alignment on the grid
-            hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
-            //Add login button to the button box
-            hbBtn.getChildren().add(btnStart);
-            //Add button box to the grid(root) element on the column/line index
-            grid.add(hbBtn, 1, 7);
-
-
-
-
+            Button btnStart = new Button("START!");
             btnStart.setOnAction(new EventHandler<ActionEvent>() {
                 @Override
-                public void handle(ActionEvent event) {//implements the method that will be called
-                    controller.sevePosition();
-                }
+                public void handle(ActionEvent event) {
+                    ManagerScheduleView mv = new ManagerScheduleView();
 
+                    try {
+                        mv.start(stage);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
             });
 
+            //sets styles
 
+            // adds components to the vboxes
+            vboxContents.getChildren().addAll(title, grid, positionTitle, table, addPositionName, addButton, btnStart);
+
+            // sets alignment
+            vboxContainer.setAlignment(Pos.TOP_CENTER);
+            vboxContents.setAlignment(Pos.CENTER);
+
+            // sets components
+            pane.setTop(vboxContainer);
+            pane.setCenter(vboxContents);
+
+
+            // creates scene
+            Scene scene = new Scene(pane, 1280, 800);
+
+            //Import general css file
+            scene.getStylesheets().add(new URL("file:resources/css/application.css").toExternalForm());
+
+            //pane.setId("set-start-date");
+
+
+            //Add scene to the stage
             stage.setScene(scene);
+            //Show stage
             stage.show();
-
-
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
+        }
+        {
         }
     }
 }
