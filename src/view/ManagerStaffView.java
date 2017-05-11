48888c92deffdf59d31ae00e1ff6a878abd43203
MinaFujisawa <fujimina37@gmail.com>
2017-05-10 22:48:18 -0700

    add style for MagerStaffView

--- src/view/ManagerStaffView.java
+++ src/view/ManagerStaffView.java
@@ -8,13 +8,15 @@ import controller.ManagerStaffController;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.VPos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
-import javafx.scene.control.Label;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Modality;
@@ -29,7 +31,7 @@ import java.net.URL;
 public class ManagerStaffView extends View {
 
     private ManagerStaffController controller = new ManagerStaffController();
-    int index = 2;
+    int index = 3;
 
     @Override
     public void start(Stage stage) {
@@ -39,11 +41,13 @@ public class ManagerStaffView extends View {
             VBox vboxContainer = new VBox();
             GridPane grid = new GridPane();
             grid.getStyleClass().add("grid");
-            grid.setHgap(10);
-            grid.setVgap(10);
-            grid.getColumnConstraints().add(new ColumnConstraints(300)); // column 1
-            grid.getColumnConstraints().add(new ColumnConstraints(300)); // column 2
-            grid.getColumnConstraints().add(new ColumnConstraints(300)); // column 3
+            grid.setHgap(20);
+            grid.setVgap(20);
+            grid.getColumnConstraints().add(new ColumnConstraints(250)); // column 1
+            grid.getColumnConstraints().add(new ColumnConstraints(250)); // column 2
+            grid.getColumnConstraints().add(new ColumnConstraints(250)); // column 3
+            grid.setPadding(new Insets(50, 0, 0, 0));
+
 
 
             //creates components
@@ -51,13 +55,21 @@ public class ManagerStaffView extends View {
             Label hID = new Label("ID");
             Label hPassword = new Label("PASSWORD");
 
-            Button addStaffBtn = new Button("Add Staff");
+            Image iconPlus = new Image("file:resources/images/button_plus.png");
+            ImageView iconPlusView = new ImageView(iconPlus);
+            Button addStaffBtn = new Button("Add Staff", iconPlusView);
+            addStaffBtn.getStyleClass().add("addStaffBtn");
+
+            Separator border = new Separator();
 
             //table layout
-            grid.add(addStaffBtn, 0, 0, 4, 1);
-            grid.add(hName, 0, 1, 1, 1);
-            grid.add(hID, 1, 1, 1, 1);
-            grid.add(hPassword, 2, 1, 1, 1);
+            grid.add(addStaffBtn, 0, 0, 3, 1);
+            grid.add(border, 0, 1, 3, 1);
+            grid.add(hName, 0, 2, 1, 1);
+            grid.add(hID, 1, 2, 1, 1);
+            grid.add(hPassword, 2, 2, 1, 1);
+
+
 
             //button
             addStaffBtn.setOnAction(new EventHandler<ActionEvent>() {
@@ -76,42 +88,36 @@ public class ManagerStaffView extends View {
                          * Contents in the modal window
                          ***************/
                         //creates root elements
+                        VBox vbModal = new VBox();
+
                         GridPane gridModal = new GridPane();
-                        gridModal.setHgap(10);
+                        gridModal.setHgap(20);
                         gridModal.setVgap(10);
-                        gridModal.getColumnConstraints().add(new ColumnConstraints(100)); // column 1
-                        gridModal.getColumnConstraints().add(new ColumnConstraints(100)); // column 2
+//                        gridModal.getColumnConstraints().add(new ColumnConstraints(100)); // column 1
+//                        gridModal.getColumnConstraints().add(new ColumnConstraints(100)); // column 2
+                        gridModal.setPadding(new Insets(50, 50, 50, 50));
+//                        gridModal.setMinWidth(500);
+
 
                         //creates components
+                        Text modTitle = new Text("ADD STAFF");
+                        modTitle.setFont(new Font(20));
+
                         Text hName = new Text("NAME");
                         Text hPosi = new Text("POSITION");
-                        Button addStaffBtn = new Button("Add Staff");
                         TextField nameField = new TextField();
-                        Position position = new Position();
-                        Long positionID = position.getId();
+//                        Position position = new Position();
+                        Separator border2 = new Separator();
 
                         ChoiceBox cb = new ChoiceBox(FXCollections.observableList(controller.getAllPositions()));
-                        System.out.println(controller.getAllPositions());
+                        cb.getStyleClass().add("choiceBox");
 
-
-                        //Styles
-                        gridModal.setMinWidth(500);
-
-                        //table layout
-                        gridModal.add(hName, 0, 0, 1, 1);
-                        gridModal.add(nameField, 1, 0, 1, 1);
-                        gridModal.add(hPosi, 0, 1, 1, 1);
-                        gridModal.add(cb, 1, 1, 1, 1);
-                        gridModal.add(addStaffBtn, 0, 2, 3, 1);
-
-                        addStaffBtn.setOnAction(new EventHandler<ActionEvent>() {
+                        Button addBtn = new Button("Add Staff");
+                        addBtn.setOnAction(new EventHandler<ActionEvent>() {
                             @Override
                             public void handle(ActionEvent event) {
-//                                ManagerStaffView ms = new ManagerStaffView();
-
                                 try {
                                     Text addedName = new Text();
-                                    Text test = new Text("Test");
                                     addedName.setText(nameField.getText());
                                     Text id = new Text();
                                     id.setText(controller.getSaltString());
@@ -127,7 +133,46 @@ public class ManagerStaffView extends View {
                             }
                         });
 
-                        Scene winScene = new Scene(gridModal, 400, 400);
+                        Button cancelBtn = new Button("Cancel");
+                        cancelBtn.getStyleClass().add("subButton");
+
+                        cancelBtn.setOnAction(new EventHandler<ActionEvent>() {
+                            @Override
+                            public void handle(ActionEvent event) {
+//                                ManagerStaffView ms = new ManagerStaffView();
+
+                                try {
+                                    winStage.close();
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        });
+
+                        //table layout
+                        gridModal.add(hName, 0, 0, 1, 1);
+                        gridModal.add(nameField, 1, 0, 3, 1);
+                        gridModal.add(hPosi, 0, 1, 1, 1);
+                        gridModal.add(cb, 1, 1, 3, 1);
+                        gridModal.add(border2, 0, 2, 4, 1);
+                        gridModal.add(cancelBtn, 2, 3, 1, 1);
+                        gridModal.add(addBtn, 3, 3, 1, 1);
+
+
+                        VBox.setMargin(modTitle, new Insets(50, 0, 0, 0));
+                        vbModal.setAlignment(Pos.TOP_CENTER);
+
+
+                        vbModal.getChildren().addAll(modTitle, gridModal);
+
+                        Scene winScene = new Scene(vbModal);
+
+                        //Import general css file
+                        winScene.getStylesheets().add(new URL("file:resources/css/application.css").toExternalForm());
+
+                        GridPane.setMargin(border2, new Insets(20, 0, 10, 0));
+
+
                         winStage.setScene(winScene);
                         winStage.show();
 
@@ -159,6 +204,7 @@ public class ManagerStaffView extends View {
 
             //Import general css file
             scene.getStylesheets().add(new URL("file:resources/css/application.css").toExternalForm());
+
 
             pane.setId("staff");
 
